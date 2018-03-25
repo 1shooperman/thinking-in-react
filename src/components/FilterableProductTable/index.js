664841b93dd572d93adcd30d2e6ef37546b76b0a
Brandon Shoop <brandon.shoop@boats.com>
2018-03-25 19:55:05 -0400

    Adding component structure

---
+++ src/components/FilterableProductTable/index.js
@@ -0,0 +1,5 @@
+import React, {Component} from 'react';
+
+class FilterableProductTable extends Component {
+  
+}
---
+++ src/components/ProductTable/ProductCategoryRow/index.js
@@ -0,0 +1,5 @@
+import React, {Component} from 'react';
+
+class ProductCategoryRow extends Component {
+  
+}
